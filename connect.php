d8886bb0c2c381534ef6ef3d2c2690cb3692a8e9
Shelma Imara Bakir <86011698+shelmaimara3@users.noreply.github.com>
2022-05-23 13:10:27 +0700

    Add files via upload
    Perubahan koneksi database online

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $base_url  = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
 $base_url .= "://".$_SERVER['HTTP_HOST'];
 $base_url .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
-$connection = mysqli_connect("localhost","root","","inventaris");
+$connection = mysqli_connect("sql104.byethost7.com","b7_31783816","qwerty567","b7_31783816_inventaris");
 if(!$connection){
   die("Koneksi Gagal: ". mysqli_connect_error());
   exit();
